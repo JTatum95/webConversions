91862197c6f0bc032b114f4b5fdadf48c68bd0fc
JTatum95 <Jessetat95@gmail.com>
2017-08-08 10:39:15 -0500

    Added emto%, emtopx, and updated accounting for user error

---
+++ installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp pxtoem /usr/local/sbin/
+cp emtopx /usr/local/sbin/
+cp pxtorem /usr/local/sbin/
+cp emto% /usr/local/sbin/
+
+source ~/.bash_profile
+
+echo "Provided that no errors have occured, you are now able to run these scripts from anywhere!"
